d55a736336debd1b23ecebc1c362d88ac5ce86e2
rupeshknn <rupeshknn@gmail.com>
2022-10-19 08:11:10 +0530

    fineAmp runs okay but x_amp does not get updated so no change in 2nd run

--- fineamp.py
+++ fineamp.py
@@ -25,9 +25,13 @@ from qiskit.providers.models.pulsedefaults import Command
 
 from qiskit_experiments.exceptions import QiskitError
 # %%
-IBMQ.load_account()
-provider = IBMQ.get_provider(hub="ibm-q", group="open", project="main")
-backend = provider.get_backend("ibmq_lima")
+# IBMQ.load_account()
+# provider = IBMQ.get_provider(hub="ibm-q", group="open", project="main")
+# h_backend = provider.get_backend("ibmq_lima")
+# %%
+
+from qiskit_experiments.test.iq_pulse_backend import SingleTransmonTestBackend
+pulse_backend = SingleTransmonTestBackend()
 # %%
 from qiskit_experiments.calibration_management.calibrations import Calibrations
 from qiskit_experiments.calibration_management.basis_gate_library import (
@@ -36,15 +40,34 @@ from qiskit_experiments.calibration_management.basis_gate_library import (
 
 qubit = 0
 library = FixedFrequencyTransmon()
-cals = Calibrations.from_backend(backend, libraries=[library])
-# %%
-
-from qiskit_experiments.test.iq_pulse_backend import SingleTransmonTestBackend
-pulse_backend = SingleTransmonTestBackend()
+cals = Calibrations.from_backend(pulse_backend, libraries=[library])
 # %%
 
 from qiskit_experiments.library.calibration.fine_amplitude import FineXAmplitudeCal
-amp_x_cal = FineXAmplitudeCal(qubit, cals, backend=backend, schedule_name="x")
+amp_x_cal = FineXAmplitudeCal(qubit, cals, backend=pulse_backend, schedule_name="x")
 # %%
 amp_x_cal.circuits()[3].draw()
+print(amp_x_cal.backend)
+# %%
+
+data_fine = amp_x_cal.run().block_for_results()
+
+# %%
+data_fine.figure(0)
+# %%
+print(data_fine.analysis_results("d_theta"))
+
+# %%
+dtheta = data_fine.analysis_results("d_theta").value.nominal_value
+target_angle = np.pi
+scale = target_angle / (target_angle + dtheta)
+pulse_amp = cals.get_parameter_value("amp", qubit, "x")
+print(f"The ideal angle is {target_angle:.2f} rad. We measured a deviation of {dtheta:.3f} rad.")
+print(f"Thus, scale the {pulse_amp:.4f} pulse amplitude by {scale:.3f} to obtain {pulse_amp*scale:.5f}.")
+# %%
+pd.DataFrame(**cals.parameters_table(qubit_list=[qubit, ()], parameters="amp"))
+
+# %%
+data_fine2 = amp_x_cal.run().block_for_results()
+data_fine2.figure(0)
 # %%
