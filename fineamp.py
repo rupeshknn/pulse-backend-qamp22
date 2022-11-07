146d0af82a5eea4ba7e23f9e31f1556a52a63c50
rupeshknn <rupeshknn@gmail.com>
2022-11-07 18:40:42 +0530

    sync

--- fineamp.py
+++ fineamp.py
@@ -2,28 +2,29 @@
 # %load_ext autoreload
 # %autoreload 2
 # %%
-import datetime
+# import datetime
+# from enum import Flag
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 plt.rcParams["font.size"] = 16
 
-from qiskit import QuantumCircuit, IBMQ, schedule
-import qiskit.pulse as pulse
-from qiskit.quantum_info.states import Statevector, DensityMatrix
-from qiskit_dynamics import Solver, Signal
-from qiskit_dynamics.pulse import InstructionToSignals
-from qiskit.providers import BackendV2, QubitProperties
-from qiskit.providers.options import Options
-from qiskit.transpiler import Target
+# from qiskit import QuantumCircuit, IBMQ, schedule
+# import qiskit.pulse as pulse
+# from qiskit.quantum_info.states import Statevector, DensityMatrix
+# from qiskit_dynamics import Solver, Signal
+# from qiskit_dynamics.pulse import InstructionToSignals
+# from qiskit.providers import BackendV2, QubitProperties
+# from qiskit.providers.options import Options
+# from qiskit.transpiler import Target
 
 
-from qiskit.providers.models import PulseDefaults
-from qiskit.qobj.pulse_qobj import PulseLibraryItem, PulseQobjInstruction
-from qiskit.providers.models.pulsedefaults import Command
+# from qiskit.providers.models import PulseDefaults
+# from qiskit.qobj.pulse_qobj import PulseLibraryItem, PulseQobjInstruction
+# from qiskit.providers.models.pulsedefaults import Command
 
-from qiskit_experiments.exceptions import QiskitError
+# from qiskit_experiments.exceptions import QiskitError
 # %%
 # IBMQ.load_account()
 # provider = IBMQ.get_provider(hub="ibm-q", group="open", project="main")
@@ -31,7 +32,7 @@ from qiskit_experiments.exceptions import QiskitError
 # %%
 
 from qiskit_experiments.test.iq_pulse_backend import SingleTransmonTestBackend
-pulse_backend = SingleTransmonTestBackend()
+pulse_backend = SingleTransmonTestBackend(noise=False)
 # %%
 from qiskit_experiments.calibration_management.calibrations import Calibrations
 from qiskit_experiments.calibration_management.basis_gate_library import (
@@ -40,17 +41,33 @@ from qiskit_experiments.calibration_management.basis_gate_library import (
 
 qubit = 0
 library = FixedFrequencyTransmon()
-cals = Calibrations.from_backend(pulse_backend, libraries=[library])
+cals = Calibrations.from_backend(pulse_backend)# libraries=[library])
+cals.parameters_table()
 # %%
 
 from qiskit_experiments.library.calibration.fine_amplitude import FineXAmplitudeCal
-amp_x_cal = FineXAmplitudeCal(qubit, cals, backend=pulse_backend, schedule_name="x")
+from qiskit_experiments.library.characterization import FineXAmplitude
+
+# amp_x_cal = FineXAmplitudeCal(qubit, cals, backend=pulse_backend, schedule_name="x")
+amp_x_cal = FineXAmplitude(qubit, backend=pulse_backend)
+
+
 # %%
-amp_x_cal.circuits()[3].draw()
-print(amp_x_cal.backend)
+amp_x_cal._transpiled_circuits()[3].calibrations
+
+# print(amp_x_cal.backend)
 # %%
 
 data_fine = amp_x_cal.run().block_for_results()
+
+# %%
+
+# list(pulse_backend._schedule_cache.keys())
+data_fine.figure(0)
+# %%
+
+cals.parameters_table()['data']
+
 
 # %%
 data_fine.figure(0)
